d1d6c6b9551e922b8628768427dd1503dae958ee
Ravannan777 <ksreenand708@gmail.com>
2026-08-11 18:37:40 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ls-tuition-v1.01';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/result-admin.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch Cached Files when Offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
